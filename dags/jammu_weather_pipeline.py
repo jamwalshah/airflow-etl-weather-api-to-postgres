a41b47d6142c45bf44488bb0eb3251301765ce8e
jamwalshah <suryadevsinghjamwal01@gmail.com>
2024-10-31 20:14:43 +0530

    Updated jammu weather etl DAG : implemented Extract, Transform, Load tasks

--- dags/jammu_weather_pipeline.py
+++ dags/jammu_weather_pipeline.py
@@ -1,6 +1,10 @@
+from decouple import config
 from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.decorators import task
+from airflow.providers.http.hooks.http import HttpHook
+import requests
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 ## Location params
 JAMMU = {# 'timezone':'IST',
@@ -13,16 +17,17 @@ JAMMU = {# 'timezone':'IST',
 ## Connection Names
 
 # to set API HOST for HttpHook
-API_CONN_ID = 'open_meteo_api'
+# API_CONN_ID = 'open_meteo_api'
+API_CONN_ID = config('API_CONN')
 
 # for test/dev using postgres on docker
-POSTGRES_CONN_ID = 'postgres_docker'
+POSTGRES_CONN_ID = config('POSTGRES_CONN')
 
 # to be used to dump data into AWS RDS PostgreSQL
-AWS_RDS_CONN_ID = 'aws_rds_default'
+AWS_RDS_CONN_ID = config('AWS_RDS_CONN')
 
 # to be used to dump data into Azure Database for PostgreSQL
-AZ_POSTGRES_CONN_ID = 'az_postgresql_default'
+AZ_POSTGRES_CONN_ID = config('AZ_POSTGRES_CONN')
 
 
 ## DAG default args
@@ -30,11 +35,6 @@ dag_default_args = {
     'owner':'airflow',
     'start_date':days_ago(n=1)
 }
-
-
-## API endpoint
-# API_HOST = 'https://api.open-meteo.com/'
-api_endpoint = f'/v1/forecast?latitude={JAMMU["latitude"]}&longitude={JAMMU["longitude"]}&current_weather=true'
 
 # Directed Acyclic Graph (DAG)
 
@@ -45,18 +45,91 @@ with DAG(dag_id='jammu_weather',
     
     @task()
     def extract_jammu_weather():
-        pass
+        # Use HttpHook from airflow providers Http to get Http Connection
+        http_hook = HttpHook(method='GET', http_conn_id=API_CONN_ID)
+        
+        # Weather API endpoint
+        # API_HOST = 'https://api.open-meteo.com/'
+        api_endpoint = f'/v1/forecast?latitude={JAMMU["latitude"]}&longitude={JAMMU["longitude"]}&timezone={JAMMU["timezone"]}&current_weather=true'
+        
+        # Perform request using HttpHook
+        api_response = http_hook.run(endpoint=api_endpoint)
+        
+        if api_response.status_code == 200:
+            return api_response.json()
+        else:
+            raise Exception(f'Unsuccessful extraction from weather data :\
+                {api_response.status_code}')
     
     @task()
-    def transform_jammu_weather(extracted_jammu_weather_data):
-        pass
+    def transform_jammu_weather(extracted):
+        
+        current_weather = extracted['current_weather']
+        transformed = {
+            'longitude': extracted['longitude'],
+            'latitude': extracted['latitude'],
+            'temperature': current_weather['temperature'],
+            'windspeed': current_weather['windspeed'],
+            'winddirection': current_weather['winddirection'],
+            'weathercode': current_weather['weathercode'],
+            'timezone': extracted['timezone_abbreviation'],
+            'timestamp': current_weather['time']
+        }
+        return transformed
+        
     
     @task()
-    def load_jammu_weather(transformed_jammu_weather_data):
-        pass
+    def load_jammu_weather(transform):
+
+        # connections = [POSTGRES_CONN_ID, AWS_RDS_CONN_ID, AZ_POSTGRES_CONN_ID]
+        # connections = [POSTGRES_CONN_ID, AZ_POSTGRES_CONN_ID]
+        # connections = [POSTGRES_CONN_ID, AWS_RDS_CONN_ID]
+        connections = [POSTGRES_CONN_ID]
+        for conn_id in connections:
+            # Use PostgreSQL hook from Airflow providers PostgresHook
+            # to get PostgreSQL connection
+            pg_hook = PostgresHook(postgres_conn_id=conn_id)
+            
+            # Create DB connection and cursor
+            cnx = pg_hook.get_conn()
+            crsr = cnx.cursor()
+            
+            # Create table if not exists
+            query_str = """CREATE TABLE IF NOT EXISTS weather_jammu_hourly (
+                latitude FLOAT,
+                longitude FLOAT,
+                temperature FLOAT,
+                windspeed FLOAT,
+                winddirection FLOAT,
+                weathercode INT,
+                timezone VARCHAR(50),
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );"""
+            crsr.execute(query=query_str)
+            
+            # Insert transformed data into PostgreSQL table
+            query_str = """INSERT INTO weather_jammu_hourly
+                            (latitude, longitude, temperature, windspeed,
+                            winddirection, weathercode, timezone, timestamp)
+                        VALUES
+                            (%s, %s, %s, %s, %s, %s, %s, %s)"""
+            query_params = (transform['latitude'],
+                            transform['longitude'],
+                            transform['temperature'],
+                            transform['windspeed'],
+                            transform['winddirection'],
+                            transform['weathercode'],
+                            transform['timezone'],
+                            transform['timestamp'])
+            crsr.execute(query=query_str, vars=query_params)
+            
+            # Close DB connection and cursor
+            cnx.commit()
+            cnx.close()
+        
     
     # DAG Workflow
     extracted_jammu_weather_data = extract_jammu_weather()
-    transformed_jammu_weather_data = transform_jammu_weather(extracted_jammu_weather_data=extracted_jammu_weather_data)
-    load_jammu_weather(transformed_jammu_weather_data=transformed_jammu_weather_data)
+    transformed_jammu_weather_data = transform_jammu_weather(extracted=extracted_jammu_weather_data)
+    load_jammu_weather(transform=transformed_jammu_weather_data)
     
